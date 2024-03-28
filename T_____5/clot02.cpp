d169d00708e76f8297ea314b2a353b3083eac3d1
Anna1112333 <anna_1112333@mail.ru>
2024-03-29 00:08:35 +0500

    thema кнопка не меняет цвет фона

--- T_____5/clot02.cpp
+++ T_____5/clot02.cpp
@@ -14,6 +14,19 @@ qDebug() << "Получен Signals_ss";
 }
 void Clot02::SendSignal_r()
 {
-qDebug() << "Получен Signals_r";
+qDebug() << "Получен Signals_r  "<<qVersion();
     emit sig_pb_r();
 }
+void Clot02::SendSignal_time()
+{
+qDebug() << "Получен Signals_time  ";
+}
+
+void Clot02::setvalue(bool val)
+{Clot02::value1=val;}
+
+void Clot02::SendSignal_tema(){  //****
+    emit sig_pb_tema();
+
+}
+//bool Clot02::value()  {return Clot02::value1;}
